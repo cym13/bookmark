ea35e1eca6804b6581f0a89bbc18d8816c4f5258
Cédric Picard <mystiro@gmail.com>
2014-06-29 02:03:00 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -61,5 +61,8 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['docopt', 'msgpack-python']
+    install_requires=['docopt',
+                      'msgpack-python',
+                      'requests'
+                     ]
 )

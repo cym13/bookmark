807e61a204160e1636930140aa45bb3a835986b6
Cédric Picard <cedric.picard@efrei.net>
2015-01-01 19:29:04 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version="1.5.1",
+    version="1.5.2",
 
     description='Simple command line bookmark and/or tagging utility.',
 
